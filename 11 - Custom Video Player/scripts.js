21b0c62da0e336a8db262136df9b1ea4c274f72a
Andrew <andrewdblum@gmail.com>
2018-03-29 14:32:35 -0700

    custom video player

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -4,7 +4,7 @@ const prog = document.querySelector('.progress')
 const progfill = document.querySelector('.progress__filled')
 const toggle = document.querySelector('.toggle')
 const skipButtons = document.querySelectorAll('[data-skip]')
-const ranges = document.querySelector('.player__slider')
+const ranges = document.querySelectorAll('.player__slider')
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
@@ -16,10 +16,22 @@ function updateButton() {
 function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
-
+function handleSeek(e) {
+  video[this.name] = this.value;
+}
+function handleProgress() {
+  let currentTime = (video.currentTime / video.duration) * 100;
+  progfill.style.flexBasis = `${currentTime}%`;
+}
+function seek(e) {
+  video.currentTime = parseFloat((e.offsetX / prog.offsetWidth) * video.duration);
+}
 
 toggle.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 toggle.addEventListener('click', updateButton);
 video.addEventListener('click', updateButton);
 skipButtons.forEach(e => e.addEventListener('click', skip));
+ranges.forEach(e => e.addEventListener('change', handleSeek));
+video.addEventListener('timeupdate', handleProgress);
+prog.addEventListener('click', seek);
